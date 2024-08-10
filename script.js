3085eb2d2915ba8c266236e421b990eb2be52d5d
aj-cabreros <174784983+aj-cabreros@users.noreply.github.com>
2024-08-09 21:24:42 -1000

    [+] Complete full functioning game via prompts and alerts only
    Looking at the step 6 of the odin instructions. It says to move playRound and the score variables inside the new playGame function. I don't like that implimentation. To retain easy ability to repeat the playGame function and change the amount of rounds per game, I kept both functions seperate.

--- script.js
+++ script.js
@@ -1,5 +1,22 @@
 
 
+let humanScore = 0;
+let computerScore = 0;
+let ties = 0;
+let playAgain;
+
+do{
+    alert("Let's play rock, paper, scissors!");
+    playGame();
+
+    playAgain = window.prompt("Play Again? Type 'no' to exit.")
+    playAgain = playAgain.toLowerCase();
+
+} while (playAgain !== "no")
+
+
+
+// Past this are all the function definitions
 function getComputerChoice(){
    let value = Math.random();
     //console.log(value);
@@ -15,14 +32,90 @@ function getComputerChoice(){
 function getHumanChoice(){
     let choice;
 
-    do{ choice = window.prompt("Type rock, paper, or scissors in all lowercase");}
+    do{ choice = window.prompt("Type rock, paper, or scissors");
+        choice = choice.toLowerCase();
+    }
     while(choice !== "rock" && choice !== "paper" && choice !== "scissors");
 
     return choice;
 }
 
+function playRound(){
+
+    let humanChoice = getHumanChoice();
+    let computerChoice = getComputerChoice();
+
+    //Depending on humanChoice, who wins and update score
+    if(humanChoice === "rock"){
+        switch(computerChoice){
+            case "rock":
+                alert("It's a tie! Rock vs Rock!");
+                ties++;
+                break;
+            case "paper":
+                alert("You lose! Paper beats Rock!");
+                computerScore++;
+                break;
+            case "scissors":
+                alert("You win! Rock beats Scissors!");
+                humanScore++;
+                break;
+        }
+    }
+
+    if(humanChoice === "paper"){
+        switch(computerChoice){
+            case "paper":
+                alert("It's a tie! Paper vs Paper!");
+                ties++;
+                break;
+            case "scissors":
+                alert("You lose! Scissors beats Paper!");
+                computerScore++;
+                break;
+            case "rock":
+                alert("You win! Paper beats Rock!");
+                humanScore++;
+                break;
+        }
+    
+    }
+
+    if(humanChoice === "scissors"){
+        switch(computerChoice){
+            case "scissors":
+                alert("It's a tie! Scissors vs Scissors!");
+                ties++;
+                break;
+            case "rock":
+                alert("You lose! Rock beats Scissors!");
+                computerScore++;
+                break;
+            case "paper":
+                alert("You win! Scissors beats Paper!");
+                humanScore++;
+                break;
+        }
+    
+    }
+}
+
+function playGame(){
+    for(round = 0; round < 5 ; round++){
+        alert("Ties: " + ties + "   Your wins: " + humanScore + "   CPU wins: " + computerScore)
+        playRound();
+    }
+    if (humanScore > computerScore){
+        alert("Ties: " + ties + "   Your wins: " + humanScore + "   CPU wins: " + computerScore + "         YOU WIN!")  
+    } else if(humanScore < computerScore) {
+        alert("Ties: " + ties + "   Your wins: " + humanScore + "   CPU wins: " + computerScore + "         You lost...!")
+    } else {
+        alert("Ties: " + ties + "   Your wins: " + humanScore + "   CPU wins: " + computerScore + "         Wow a Tie?!")
+    }
+
+    humanScore = 0;
+    computerScore = 0;
+    ties = 0;
 
 
-
-console.log("You throw out: " + getHumanChoice());
-console.log("Computer throws out: " + getComputerChoice());
+}
