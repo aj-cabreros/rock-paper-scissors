3ee26c80e39dffc516b69a72cd2f9af18f3df6f2
aj-cabreros <174784983+aj-cabreros@users.noreply.github.com>
2024-08-15 19:55:49 -1000

    [+] Add basic UI to game
    Self explanatory. Added a styles.css to center the elements onto the page.

--- script.js
+++ script.js
@@ -3,20 +3,58 @@
 let humanScore = 0;
 let computerScore = 0;
 let ties = 0;
-let playAgain;
+let roundsPlayed = 0;
 
-do{
-    alert("Let's play rock, paper, scissors!");
-    playGame();
+// Initializations of DOM Manipulations =========
 
-    playAgain = window.prompt("Play Again? Type 'no' to exit.")
-    playAgain = playAgain.toLowerCase();
+let textbox = document.querySelector("#textbox");
+textbox.textContent = "Choose Rock, Paper, or Scissors to Begin";
 
-} while (playAgain !== "no")
+let textbox2 = document.querySelector("#textbox2");
+textbox2.textContent = "";
+
+let roundNumber = document.querySelector("#round");
+roundNumber.textContent = "Rounds Played: " + roundsPlayed;
+
+let score1 = document.querySelector("#score-board-1");
+score1.textContent = "Player Score: " + humanScore;
+
+let score2 = document.querySelector("#score-board-2");
+score2.textContent = "Ties: " + ties;
+
+let score3 = document.querySelector("#score-board-3");
+score3.textContent = "CPU Score: " + computerScore;
+
+let again = document.querySelector("#buttons")
+let againButton;
 
 
+// eventListeners for all three options: rock, paper, scissors
+let choice = document.querySelector('#choice');
+let humanChoice = choice.addEventListener("click", (e) => {
+    let target = e.target;
 
-// Past this are all the function definitions
+    switch(target.id) {
+        case 'rock':
+            if(roundsPlayed >= 5){
+            alert("If you wanna play again. Press the button.")
+            }else{playRound("rock");}
+            break;
+        case 'paper':
+            if(roundsPlayed >= 5){
+            alert("If you wanna play again. Press the button.")
+            }else{playRound("paper");}
+            break;
+        case 'scissors':
+            if(roundsPlayed >= 5){
+            alert("If you wanna play again. Press the button.")
+            }else{playRound("scissors");}
+            break;
+    }
+  });
+
+// ======== Past this are all the function definitions ====================
+
 function getComputerChoice(){
    let value = Math.random();
     //console.log(value);
@@ -29,36 +67,33 @@ function getComputerChoice(){
     }
 }
 
-function getHumanChoice(){
-    let choice;
+function playRound(humanChoice){
 
-    do{ choice = window.prompt("Type rock, paper, or scissors");
-        choice = choice.toLowerCase();
-    }
-    while(choice !== "rock" && choice !== "paper" && choice !== "scissors");
+    roundsPlayed++;
+    roundNumber.textContent = "Rounds Played: " + roundsPlayed;
 
-    return choice;
-}
-
-function playRound(){
-
-    let humanChoice = getHumanChoice();
     let computerChoice = getComputerChoice();
 
     //Depending on humanChoice, who wins and update score
     if(humanChoice === "rock"){
         switch(computerChoice){
             case "rock":
-                alert("It's a tie! Rock vs Rock!");
+                textbox.textContent = "It's a tie! Rock vs Rock!";
+                textbox2.textContent = "Choose Rock, Paper, or Scissors to Continue";
                 ties++;
+                score2.textContent = "Ties: " + ties;
                 break;
             case "paper":
-                alert("You lose! Paper beats Rock!");
+                textbox.textContent = "You lose! Paper beats Rock!";
+                textbox2.textContent = "Choose Rock, Paper, or Scissors to Continue";
                 computerScore++;
+                score3.textContent = "CPU Score: " + computerScore;
                 break;
             case "scissors":
-                alert("You win! Rock beats Scissors!");
+                textbox.textContent = "You win! Rock beats Scissors!";
+                textbox2.textContent = "Choose Rock, Paper, or Scissors to Continue";
                 humanScore++;
+                score1.textContent = "Player Score: " + humanScore;
                 break;
         }
     }
@@ -66,16 +101,22 @@ function playRound(){
     if(humanChoice === "paper"){
         switch(computerChoice){
             case "paper":
-                alert("It's a tie! Paper vs Paper!");
+                textbox.textContent = "It's a tie! Paper vs Paper!";
+                textbox2.textContent = "Choose Rock, Paper, or Scissors to Continue";
                 ties++;
+                score2.textContent = "Ties: " + ties;
                 break;
             case "scissors":
-                alert("You lose! Scissors beats Paper!");
+                textbox.textContent = "You lose! Scissors beats Paper!";
+                textbox2.textContent = "Choose Rock, Paper, or Scissors to Continue";
                 computerScore++;
+                score3.textContent = "CPU Score: " + computerScore;
                 break;
             case "rock":
-                alert("You win! Paper beats Rock!");
+                textbox.textContent = "You win! Paper beats Rock!";
+                textbox2.textContent = "Choose Rock, Paper, or Scissors to Continue";
                 humanScore++;
+                score1.textContent = "Player Score: " + humanScore;
                 break;
         }
     
@@ -84,38 +125,71 @@ function playRound(){
     if(humanChoice === "scissors"){
         switch(computerChoice){
             case "scissors":
-                alert("It's a tie! Scissors vs Scissors!");
+                textbox.textContent = "It's a tie! Scissors vs Scissors!";
+                textbox2.textContent = "Choose Rock, Paper, or Scissors to Continue";
                 ties++;
+                score2.textContent = "Ties: " + ties;
                 break;
             case "rock":
-                alert("You lose! Rock beats Scissors!");
+                textbox.textContent = "You lose! Rock beats Scissors!";
+                textbox2.textContent = "Choose Rock, Paper, or Scissors to Continue";
                 computerScore++;
+                score3.textContent = "CPU Score: " + computerScore;
                 break;
             case "paper":
-                alert("You win! Scissors beats Paper!");
+                textbox.textContent = "You win! Scissors beats Paper!";
+                textbox2.textContent = "Choose Rock, Paper, or Scissors to Continue";
                 humanScore++;
+                score1.textContent = "Player Score: " + humanScore;
                 break;
         }
     
     }
+
+    if(roundsPlayed == 5){
+        endGame();
+        againButton = document.createElement("button");
+        againButton.setAttribute("id", "restart");
+        againButton.textContent = "Play Again?";
+        again.appendChild(againButton);
+        
+        againButton.onclick = () => resetGame();
+
+    }
 }
 
-function playGame(){
-    for(round = 0; round < 5 ; round++){
-        alert("Ties: " + ties + "   Your wins: " + humanScore + "   CPU wins: " + computerScore)
-        playRound();
-    }
+function endGame(){
+    
+    textbox2.textContent = "Wanna play another?";
+
     if (humanScore > computerScore){
-        alert("Ties: " + ties + "   Your wins: " + humanScore + "   CPU wins: " + computerScore + "         YOU WIN!")  
+        textbox.textContent = "After 5 rounds: YOU WIN!";
     } else if(humanScore < computerScore) {
-        alert("Ties: " + ties + "   Your wins: " + humanScore + "   CPU wins: " + computerScore + "         You lost...!")
+        textbox.textContent = "After 5 rounds: You lost...!";
     } else {
-        alert("Ties: " + ties + "   Your wins: " + humanScore + "   CPU wins: " + computerScore + "         Wow a Tie?!")
+        textbox.textContent = "After 5 rounds: Wow a Tie?!";
     }
 
+    score1.textContent = "Player Score: " + humanScore;
+    score2.textContent = "Ties: " + ties;
+    score3.textContent = "CPU Score: " + computerScore;
+    roundNumber.textContent = "Rounds Played: " + roundsPlayed;
+
+}
+
+
+function resetGame(){
     humanScore = 0;
     computerScore = 0;
     ties = 0;
+    roundsPlayed = 0;
 
+    score1.textContent = "Player Score: " + humanScore;
+    score2.textContent = "Ties: " + ties;
+    score3.textContent = "CPU Score: " + computerScore;
+    roundNumber.textContent = "Rounds Played: " + roundsPlayed;
 
+    textbox.textContent = "Choose Rock, Paper, or Scissors to Begin";
+    textbox2.textContent = "";
+    again.removeChild(againButton);
 }
